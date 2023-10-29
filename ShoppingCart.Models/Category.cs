7f01d4cf38ed8b4176ff7f66e232bbe80273d9bb
dangtrantrung <trung280584@gmail.com>
2023-10-29 13:26:44 +0700

    data models

---
+++ ShoppingCart.Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.Component.DataAnotation;
+using System.Component;
+
+namespace ShoppingCart.Models;
+
+public class Category
+{
+ [Key]
+ public int Id{get;set;}
+ [Required]
+ public string Name {get;set;}
+ [DisplayName("Display Order")]
+ public int DisplayOrder {get;set;}
+ public DateTime CreatedDateTime {get;set;}=DateTime.Now;
+}
---
+++ ShoppingCart.Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Component.DataAnotation;
+using System.Component;
+
+namespace ShoppingCart.Models;
+
+public class Product
+{
+ [Key]
+ public int Id{get;set;}
+ [Required]
+ public string Name {get;set;}
+ [Required]
+ public string Description {get;set;}
+ [Required]
+ public double Price {get;set;}
+ [ValidateNever]
+ public string ImgUrl {get;set;}
+ [ValidateNever]
+ public Category Category {get;set;}
+
+}
